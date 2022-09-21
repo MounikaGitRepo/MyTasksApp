99937aea4e523e36d1586c888b4f8a81570d4df1
MounikaGitRepo <110450005+MounikaGitRepo@users.noreply.github.com>
2022-09-21 13:59:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 data = {
   username: "mounika",
   password: "mounika@2022",
 };
+app.use(express.json());
+app.use(cors());
 
 app.get("/login", (request, response) => {
-  const {username , password } = request.body;
-  
-  if(username == data.username && password == data.password) {
-     response.status(200);
-  }
-  else{
-  response.send("username or password not matched");
+  const { username, password } = request.body;
+
+  if (username == data.username && password == data.password) {
+    response.status(200);
+  } else {
+    response.send("username or password not matched");
   }
 });
 
